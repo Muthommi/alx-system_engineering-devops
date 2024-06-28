03a0479ef5fff6cba62ac1bd8a062190c8f6a94a
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-06-28 05:34:07 -0700

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # This puppet manifest creates a file with certain requirements.
 file { '/tmp/school':
   ensure  => 'file',
-  content => 'I love puppet',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
