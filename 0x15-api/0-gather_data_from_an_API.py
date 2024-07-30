5f8556e7305b89887782a69731092109cb906bd7
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-07-30 14:06:10 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,15 +7,38 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    """
-    Main entry point for the script
-    """
-    url = "https://jsonplaceholder.typicode.com/"
-    user_id = sys.argv[1]
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit
+
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    user_url = "{}/users/{}".format(base_url, employee_id)
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+
+    if not user_data:
+        print("Employee ID not found.")
+        sys.exit(1)
+
+    employee_name = user_data.get('name')
+
+    todos_url = "{}/todos".format(base_url)
+    todos_response = requests.get(todos_url, params={'userId': employee_id})
+    todos_data = todos_response.json()
+
+    total_tasks = len(todos_data)
+    done_tasks = [todo for todo in todos_data if todo.get('completed')]
+    number_of_done_tasks = len(done_tasks)
+
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+        employee_name, number_of_done_tasks, total_tasks))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
