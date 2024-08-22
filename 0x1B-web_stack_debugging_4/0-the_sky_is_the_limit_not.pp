421de50d13443b4d4af04e9d2d3f5324f814e821
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-08-22 11:53:19 +0300

    Sky is the limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# A puppet manifest that configures Nginx to handle high traffic loads
+
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  content => template('nginx/nginx.conf,erb'),
+  notify  => Exec['reload=nginx'],
+}
+
+exec { 'reload-nginx':
+  command     => 'service nginx reload',
+  refreshonly => true,
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+This is a readme file of the project web-stack debugging #4
