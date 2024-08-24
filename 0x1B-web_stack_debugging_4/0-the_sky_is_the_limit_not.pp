5d80e706a2509ce4096d8d728e07357ad004c292
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-08-24 09:39:24 +0300

    Sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,18 @@
 # A puppet manifest that configures Nginx to handle high traffic loads
 
-exec { 'fix-for-nginx':
-  command =>  'sed -i "s/[0-9]\+\s\+[0-9]\+/4096 4096/" /etc/default/nginx && service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => 'test -f /etc/default/nginx',
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  source  => 'puppet:///modules/nginx/nginx.conf',
+  notify  => Service['nginx'],
+}
+
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  content => template('nginx/nginx.conf.erb'),
+  notify  => Service['nginx'],
 }
 
 service { 'nginx':
   ensure  => running,
   enable  => true,
-  require => File['/etc/nginx/nginx.conf'],
 }
